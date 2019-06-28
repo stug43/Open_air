c57f88d24059571e8dae9f7e70212a557e0e5d53
Nic0b <nicob69@ymail.com>
2019-06-28 13:31:37 +0200

    fix:controller topic

--- app/controllers/topics_controller.rb
+++ app/controllers/topics_controller.rb
@@ -8,7 +8,7 @@ class TopicsController < ApplicationController
   def show
     @atmos = AtmoSud.new
     @atmo_city = AtmoSud.new.city_air_indice
-    @chart = (@atmos.to_i / 10)*10 + 5
+    @chart = @atmos
 
     @topic = Topic.with_attached_main_picture.find(params[:id])
     @related_topics = (Topic.all - [@topic]).sample(3)
