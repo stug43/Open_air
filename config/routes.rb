9dd3db2669417429c93e2e8a9ff4f67d2804d0e2
Auguste Nganta <nganta.a@hotmail.fr>
2019-07-28 22:36:50 +0200

    remove railsAdmin routing and route admin namespace

--- config/routes.rb
+++ config/routes.rb
@@ -5,8 +5,12 @@ Rails.application.routes.draw do
 	namespace :admin do
 		root to: 'topics#index'
   	resources :users, only: [:index, :edit, :update, :destroy]
-		resources :topics
+		resources :topics, only: [:index, :create, :edit, :update, :destroy]
+		resources :datasets, only: [:index, :create, :edit, :update, :destroy]
 	end
+	resources :users do
+    resource 'dataset_collection', only: %i[show create destroy]
+  end
   root to: 'topics#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
