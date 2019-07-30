34bc7df2a271ff3083f6d4f3ef070c45dd3cf6fe
Auguste Nganta <nganta.a@hotmail.fr>
2019-07-30 15:35:56 +0200

    code to refactor soon

--- app/controllers/searchs_controller.rb
+++ app/controllers/searchs_controller.rb
@@ -1,24 +1,15 @@
 class SearchsController < ApplicationController
     def index
+			if params[:mySuperParam]  
+      	@test = AtmoSud.new.get_api(params[:mySuperParam].to_s)
+    	end
     end
 
 	def show 
-		#@atmos = AtmoSud.new.city(params[:id])
-		#@chart = (@atmos.to_i / 10)*10 + 5
-		@test = AtmoSud.new.get_api(params[:id])
-
-
 	end
 	def create
-		#@atmos = AtmoSud.new.city(params[:search])
-		
-		@test = AtmoSud.new.get_api(params[:search])
-		redirect_to search_path(params[:search])
 	end
-
-	  def search
-    lookup = params[:lookup]
-
+	def search
   end
 
 end
